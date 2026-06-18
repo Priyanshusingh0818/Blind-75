440fab1216f25454df48186bcdfd5cc3a7d2753e
Priyanshu Singh <ps2955@srmist.edu.in>
2026-06-18 09:22:30 +0000

    Prime

---
+++ tcs/prime.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+  public:
+    bool isPrime(int n) {
+        if(n<=1){
+            return false;
+        }
+        for(int i = 2 ; i * i <=n;i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+        // code here
+        
+    }
+};
