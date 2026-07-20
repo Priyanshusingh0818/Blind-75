47981cf646f65af53e197ac3b83f89bcd4bf42f9
Priyanshu Singh <ps2955@srmist.edu.in>
2026-07-20 21:04:17 +0530

    linked list

---
+++ linked_list/detect_cycle2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        bool iscycle = false;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                iscycle = true;
+                break;
+            }
+        }
+
+        if (!iscycle) {
+            return NULL;
+        }
+
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
